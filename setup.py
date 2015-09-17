f26a6baf90c40327127e8f429fec10ce5b74c7bf
Maxim Poletaev <zenwalker2@gmail.com>
2015-09-18 00:19:05 +0300

    Update install script

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='django-goodforms',
     version='0.1.0',
-    description='Custom renderer for your form fields',
-    long_description=open('README.rst').read(),
+    description='Custom renderer for django form fields',
+    keywords='django forms',
     author='Maxim Poletaev',
     author_email='zenwalker2@gmail.com',
+    license='BSD',
     url='https://github.com/zenwalker/django-goodforms',
     packages=['goodforms', 'goodforms.templatetags'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+    ],
 )
